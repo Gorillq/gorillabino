00b18b086ef6e92bd66812d64b49bc16a1bc9a37
Gorillq <kamil.kownacki3@gmail.com>
2023-08-24 09:30:59 +0200

    fixed search

--- ssssearch/engine/urls.py
+++ ssssearch/engine/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.query, name="query"),
-    path('', views.results, name="results"),
-    path('', views.about, name="about"),
+    path('results', views.results, name="results"),
+    path('about', views.about, name="about"),
 ]
--- ssssearch/engine/views.py
+++ ssssearch/engine/views.py
@@ -15,14 +15,15 @@ def results(request):
         else:
 
             results = []
-            page = requests.get('https://search3.lycos.com/web/?q='+query).text
-            soup = BeautifulSoup(page,'lxml')
-            listings = soup.find_all(class_="result-item")
+            #type api key
+            page = requests.get('https://www.googleapis.com/customsearch/v1?key=your_api_key&cx=c380acb00f58141eb&q='+query).text
+            soup = BeautifulSoup(page)
+            listings = soup.find_all(class_="items")
             for content in listings:
-                title = content.find(class_='result-title').text
-                description = content.find(class_='result-description').text
-                link = content.find(class_='result-link').text
-                url = content.find(class_='result-url').text
+                title = content.find(class_='title').text
+                description = content.find(class_='snippet').text
+                link = content.find(class_='link').text
+                url = content.find(class_='formattedUrl').text
                 results.append((title,description,url))
             context = {
                 'results':results
